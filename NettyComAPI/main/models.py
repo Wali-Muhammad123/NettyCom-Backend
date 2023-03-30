03d07da6e3ce3ad6c77a5c91c5c100f28b845a1a
Wali-Muhammad123 <renedescartian@gmail.com>
2023-03-30 21:49:53 +0500

    Initiliazed Models and started project

---
+++ NettyComAPI/main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Agent(User):
+    pass
+class SalesData(models.Model):
+    pass
+class Teams(models.Model):
+    pass
+class TeamMembers(models.Model):
+    pass
+class Directories(models.Model):
+    pass
+class AddressCheckerUsage(models.Model):
+    pass
