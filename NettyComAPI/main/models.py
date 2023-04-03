e1540fc428a9d25b603eb1fb1a238ab363fa0af4
Wali-Muhammad123 <renedescartian@gmail.com>
2023-04-03 22:13:39 +0500

    Added User Registration Feature

--- NettyComAPI/main/models.py
+++ NettyComAPI/main/models.py
@@ -1,9 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
+from authemail.models import EmailAbstractUser, EmailUserManager
 
 # Create your models here.
-class Agent(User):
-    pass
+class Agent(EmailAbstractUser):
+    phone=models.CharField(max_length=20)
+    is_agent=models.BooleanField(default=True)
+    is_admin=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['username','phone']
+    objects=EmailUserManager()
+    def __str__(self):
+        return self.email
 class SalesData(models.Model):
     pass
 class Teams(models.Model):
