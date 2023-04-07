adf9beac0b75e578358724039e57bcee50c6e471
Wali-Muhammad123 <renedescartian@gmail.com>
2023-04-07 17:26:45 -0400

    Configured PostgreSQL to use the new database

---
+++ NettyComAPI/main/customobjects.py
@@ -0,0 +1,10 @@
+import cryptography
+class BankDetails:
+    '''
+    This class is used to store bank details of a customer.
+    '''
+    def __init__(self, account_number, bank_name):
+        self.account_number = account_number
+        self.bank_name=bank_name
+    def __str__(self) -> str:
+        return f'{self.account_number},{self.bank_name}'
--- NettyComAPI/main/models.py
+++ NettyComAPI/main/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 from authemail.models import EmailAbstractUser, EmailUserManager
-import settings
 import datetime 
 from .utils import US_STATES
+import uuid
+from django.conf import settings
+from .fields import BankDetailsField
 # Create your models here.
 class Agent(EmailAbstractUser):
+    '''
+    Custom User model for agents.
+    '''
     objects=EmailUserManager()
     def __str__(self):
         return self.email
 
-class Profile(models.Model):
+class AgentProfile(models.Model):
     user=models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone=models.CharField(max_length=20)
     level=models.IntegerField(default=1)
-    referral=models.UUIDField(auto_created=True,unique=True)
+    bank_details=BankDetailsField(max_length=100)
+    referral=models.UUIDField(auto_created=True,unique=True,default=uuid.uuid1,editable=False)
     def save(self,*args,**kwargs):
-        super(Profile,self).save(*args,**kwargs)
+        super(AgentProfile,self).save(*args,**kwargs)
 
 class SalesData(models.Model):
     STATUS=(
@@ -25,32 +31,32 @@ class SalesData(models.Model):
         ('Cancelled','Cancelled'),
         ('Completed','Completed'),
     )
-    agent=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    clientname=models.CharField(max_length=20)
-    clientphone=models.CharField(max_length=20)
+    agent=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=None)
+    clientname=models.CharField(max_length=20, default=None)
+    clientphone=models.CharField(max_length=20, default=None)
     clientemail=models.EmailField(default=None,blank=True,null=True)
-    clientaddress=models.CharField(max_length=70)
-    appointmentdate=models.DateField()
-    appointmenttime=models.TimeField()
+    clientaddress=models.CharField(max_length=70, default=None)
+    appointmentdate=models.DateField(default=datetime.date.today)
+    appointmenttime=models.TimeField(default=datetime.time(0,0))
     status=models.CharField(max_length=20,choices=STATUS,default='Pending')
     
 class Teams(models.Model):
-    teamname=models.CharField(max_length=20)
-    teamleader=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    teamname=models.CharField(max_length=20, default=None)
+    teamleader=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=None)
 class TeamMembers(models.Model):
-    team=models.ForeignKey(Teams,on_delete=models.PROTECT)
-    member=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    team=models.ForeignKey(Teams,on_delete=models.PROTECT, default=None)
+    member=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=None)
 class Directories(models.Model):
     STATES=US_STATES
-    postalcode=models.CharField(max_length=10)
-    state=models.CharField(max_length=2,choices=STATES)
-    city=models.CharField(max_length=20)
+    postalcode=models.CharField(max_length=10,default=None)
+    state=models.CharField(max_length=2,choices=STATES, default=None)
+    city=models.CharField(max_length=20, default=None)
     radius_mile=models.IntegerField(default=0)
 
 class AddressCheckerUsage(models.Model):
-    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    date=models.DateField()
-    address_requested=models.CharField(max_length=70)
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=None)
+    date=models.DateField(default=datetime.date.today)
+    address_requested=models.CharField(max_length=70, default=None)
     sale_associated=models.ForeignKey(SalesData,on_delete=models.PROTECT,default=None,blank=True,null=True)
     def save(self,*args,**kwargs):
         self.date=datetime.date.today()
