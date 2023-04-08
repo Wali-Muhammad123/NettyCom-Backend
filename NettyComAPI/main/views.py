e08f13fd24d0385e53be5c1e4f9a3d9eb080a111
Wali-Muhammad123 <renedescartian@gmail.com>
2023-04-07 20:30:04 -0400

    Did some formal debugging

--- NettyComAPI/main/views.py
+++ NettyComAPI/main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.urls import reverse
-from authemail.views import EmailVerificationView
 # Create your views here.
 def index(request):
     return render(request,{'title':'NettyCom'})
