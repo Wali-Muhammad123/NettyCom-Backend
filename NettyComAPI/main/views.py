79bb807f8bd5410326018971589e55ab1f297f92
Wali-Muhammad123 <renedescartian@gmail.com>
2023-04-08 14:02:50 -0400

    Initiliazed Address Cehcker View

--- NettyComAPI/main/views.py
+++ NettyComAPI/main/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.urls import reverse
+from rest_framework import generics, views
+from .models import Directories
 # Create your views here.
 def index(request):
     return render(request,{'title':'NettyCom'})
+
+class AddressCheckerView(views.APIView):
+    authentication_classes=[TokenAuthenticated]
+    permission_classes=[IsAuthenticated]
+    throttle_scope='addresschecker'
+    serializer_class=AddressCheckerSerializer
+    def get(self, request):
+        pass
+    def post(self, request):
+        pass 
