ea38e89387f0500f7cef9e63616fd9867579b088
Wali-Muhammad123 <renedescartian@gmail.com>
2023-04-11 18:12:45 -0400

    Added DirectorySerializer in serializers.py

--- NettyComAPI/main/serializers.py
+++ NettyComAPI/main/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import SalesData, Agent, AddressCheckerUsage
+from .models import SalesData, Agent, AddressCheckerUsage, Directories
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,8 +11,14 @@ class SaleSerializer(serializers.ModelSerializer):
         model = SalesData
         fields = '__all__'
 
-class AddressCheckerSerializer(serializers.Serializers):
+class AddressCheckerSerializer(serializers.ModelSerializer):
     user=UserSerializer(read_only=True)
     class Meta:
         model = AddressCheckerUsage
         fields='__all__'
+class DirectorySerializer(serializers.ModelSerializer):
+    coordinates=serializers.ListField(child=serializers.FloatField(),allow_blank=False,max_length=2,min_length=2)
+    class Meta:
+        model=Directories
+        fields='__all__'
+   
