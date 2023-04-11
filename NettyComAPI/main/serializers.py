16f184fff7e6bce54f17abbc88fd15bae07e54f1
Wali-Muhammad123 <renedescartian@gmail.com>
2023-04-11 18:14:17 -0400

    Extended MatrixRouter Class by adding custom methods

--- NettyComAPI/main/serializers.py
+++ NettyComAPI/main/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import SalesData, Agent, AddressCheckerUsage, Directories
+from jsonfield.encoder import JSONEncoder
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,9 +17,16 @@ class AddressCheckerSerializer(serializers.ModelSerializer):
     class Meta:
         model = AddressCheckerUsage
         fields='__all__'
+
 class DirectorySerializer(serializers.ModelSerializer):
-    coordinates=serializers.ListField(child=serializers.FloatField(),allow_blank=False,max_length=2,min_length=2)
+    coordinates=serializers.JSONField(encoder=JSONEncoder)
     class Meta:
         model=Directories
         fields='__all__'
+    def validate_coordinates(self,json_dict):
+        if type(json_dict)!=dict:
+            raise serializers.ValidationError("Coordinates must be a dictionary")
+        if 'lat' not in json_dict or 'lon' not in json_dict:
+            raise serializers.ValidationError("Coordinates must have latitude and longitude")
+        return json_dict
    
--- NettyComAPI/main/utilclasses.py
+++ NettyComAPI/main/utilclasses.py
@@ -1,6 +1,10 @@
+import json
 import requests
 from django.conf import settings
-import json 
+from rest_framework.response import Response
+from .utilfunc import geourlmaker, parsegeojson, matrixrouter, matrix_parser
+from .exceptions import FailedGeoJSONError, TimeOutError
+from .models import Directories
 class BankDetails:
     '''
     This class is used to store bank details of a customer.
@@ -23,7 +27,42 @@ class MatrixRouter:
             self.state=kwargs['state']
         if kwargs['city']:
             self.city=kwargs['city']
+        self.geocustomer,self.geocustloc=self.geocodecustomer(kwargs) #coordinates of the customer's geocode, geocustloc: Is a list that contains the state and postal code of the customer's location
+    def geocodecustomer(self,*args,**kwargs): 
+        '''
+        This method is used to geocode the customer's address. 
+        '''
+        try:
+            url=geourlmaker(kwargs)
+            response=requests.get(url=url, timeout=10)
+            geocode=parsegeojson(response)
+            if geocode[0] and geocode[1]:
+                return [geocode[0],geocode[1]],[geocode[2],geocode[3]]
+            else:
+                raise FailedGeoJSONError
+        except FailedGeoJSONError:
+            return Response({'msg':'Failed fetching geocode for customer'},status=400) 
+        except TimeoutError:
+            return Response({'msg':'Fetching GeoCode timed out.'}, status=400)
+    def fullsearch(self,*args,**kwargs):
+        queryset=Directories.objects.filter(state=self.geocustloc[0])
+        if queryset is None:
+            return Response({'msg':'We do not currently work in the state of customer address'},status=201)
+        else:
+            try:
+                json_param=matrixrouter(directories=queryset,customergeocode=self.geocustomer)
+                matrix=requests.post(
+                    url=f'https://api.tomtom.com/routing/matrix/2?key={self.API_KEY}',
+                    headers={'Content-Type' : 'application/json' },
+                    json=json_param,
+                    timeout=7
+                )
+                if matrix.status_code==200: 
+                    #more conditionals and rigorous handling of exceptions must be added here for case handling
+                    minRoute=matrix_parser(matrix.json())
+                    return minRoute
+                #rigorous handling of more http-codes must be added from here on
+            except TimeoutError:
+                raise TimeOutError
     def __str__(self) -> str:
         return f'{self.postal_code},{self.state},{self.city}'
-
-
