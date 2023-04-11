3ab80073dd98d52adbb69a87653518db4c939d13
Wali-Muhammad123 <renedescartian@gmail.com>
2023-04-11 18:13:29 -0400

    Added helper functions for matrix router in utilfunc.py

--- NettyComAPI/main/utilfunc.py
+++ NettyComAPI/main/utilfunc.py
@@ -1,10 +1,16 @@
 from django.conf import settings
+import pandas as pd
+import numpy as np
+from copy import deepcopy
+from .serializers import DirectorySerializer
+from .exceptions import InsufficientDataError
+from .utilclasses import CellError
 
 def geourlmaker(*args,**kwargs):
     #making sure that all parameters are present in kwargs
     params=['postal_code','state','city']
     for param in params:
-        if param not in kwargs.keys():
+        if param not in kwargs:
             kwargs[param]=None
     if kwargs['postal_code'] and kwargs['state'] and kwargs['city']:
         return f'https://api.tomtom.com/search/2/structuredGeocode.json?countryCode=US&municipality={kwargs["city"]}&countrySubdivision={kwargs["state"]}&postalCode={kwargs["postal_code"]}&view=Unified&key={settings.API_KEY}'
@@ -22,3 +28,69 @@ def geourlmaker(*args,**kwargs):
         return f'https://api.tomtom.com/search/2/structuredGeocode.json?countryCode=US&municipality={kwargs["city"]}&view=Unified&key={settings.API_KEY}'
     else:
         return None
+def parsegeojson(response):
+    '''
+    This function is used to parse the json response from the TomTom API.
+    '''
+    if response.status_code==200:
+        data=response.json()
+        if data['summary']['numResults']>=1:
+            return [data['results'][0]['position']['lat'], #extracting latitude 
+                    data['results'][0]['position']['lon'], #extracting longitude
+                    data['results'][0]['address']['countrySubdivision'], #extracting State
+                    data['results'][0]['address']['postalCode'] #extract postal code
+                    ]
+        else:
+            return [None,None]
+    else:
+        return [None,None]
+def matrixrouter(directories:list,customergeocode:list):
+    try:
+        json_dict={
+            "origins":[],
+            "destinations":[],
+            "options":{
+            "routeType":"fastest"
+                }
+        }
+    #Serializing the directories relevant
+        serialized_dir=DirectorySerializer(directories,many=True)
+        if serialized_dir:
+            for route in serialized_dir:
+                
+                json_dict['destinations'].append({"point":
+                                                  {"latitude":route['coordinates']['lat'],
+                                                   "longitude":route['coordinates']['lon']}})
+            json_dict['origins'].append({"point":{
+                "latitude":customergeocode[0],
+                "longitude":customergeocode[1]
+            }})
+            return json_dict
+        else:
+            raise InsufficientDataError
+    except (TypeError,ValueError):
+        return None
+    except InsufficientDataError:
+        pass
+
+def meter_to_mile(meter:float):
+    return meter/1609.344
+def matrix_parser(json_data):
+    def _helper(data):
+        if data is not np.nan:
+            return meter_to_mile(data['lengthInMeters'])
+        else:
+            return None
+    try:
+        _data=pd.json_normalize(data=json_data['data'], errors=False, max_level=0)
+        _data['routeSummary']=_data['routeSummary'].apply(_helper)
+        _dataNew=_data[_data.routeSummary.notnull()]
+        if _dataNew:
+            minRoute =_dataNew[_dataNew.routeSummary==_data.routeSummary.min()]
+            return [
+                minRoute['originIndex'],
+                minRoute['destinationIndex'],
+                minRoute['routeSummary'],
+            ]
+    except:
+        ...
