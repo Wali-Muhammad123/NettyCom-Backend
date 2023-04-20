b375828b8b71884a62e30540ee5dd98292422f89
Wali-Muhammad123 <renedescartian@gmail.com>
2023-04-20 03:52:44 -0400

    Added SaleView and TeamSalesView

--- NettyComAPI/main/views.py
+++ NettyComAPI/main/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.utils import IntegrityError
-from rest_framework import generics, views
+from rest_framework import generics, views, viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from .models import Directories, Teams
-from .serializers import TeamSerializer
+from .models import Directories, Teams, SalesData, TeamMembers
+from .serializers import TeamSerializer, SaleSerializer,UserSerializer, _SaleSerializer
 from .utilclasses import FakeNameGen
+from .permissions import IsTeamLead
+from .paginators import TeamSalesPaginater
+from .utilfunc import _saleRetriever
 # Create your views here.
 def index(request):
     return render(request,{'title':'NettyCom'})
@@ -23,7 +26,7 @@ class TeamView(generics.GenericAPIView):
         try:
             json_data={
                 "teamName":Teams.objects.get(teamleader=request.user.uuid).team_name,
-                "teamleader":request.user.uuid
+                "teamleader":request.user.uuid,
             }
         except ObjectDoesNotExist:
             return Response({"msg":"Team DoesNot Exist"},status=status.HTTP_404_NOT_FOUND)
@@ -55,3 +58,59 @@ class TeamView(generics.GenericAPIView):
                 return Response(serializer.data,status=status.HTTP_200_OK)
         except IntegrityError:
             return Response({"msg":"Something went wrong. Try again or Try Again Later."},status=status.HTTP_400_BAD_REQUEST)
+
+class SaleView(generics.GenericAPIView):
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+    throttle_scope='sales'
+    serializer_class=SaleSerializer
+    def get(self,request):
+        # Get all sales of a user code
+        queryset=SalesData.objects.filter(agent=request.user.uuid)
+        serializer=SaleSerializer(queryset,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def post(self,request, *args,**kwargs):
+        #create a sale for a user as per the sale model
+        serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    def put(self,request):
+        #update a sale for a user as per the sale model
+        serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+class TeamSales(viewsets.ViewSet):
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated, IsTeamLead]
+    serializer_class=TeamSerializer
+    pagination_class=TeamSalesPaginater
+    def list(self,request):
+        try:
+            json_data={}
+            teamId=Teams.objects.get(teamleader=request.user.uuid)
+            queryset=TeamMembers.objects.get_queryset(team=teamId)
+            if queryset is None:
+                raise ObjectDoesNotExist
+            serializer=UserSerializer(queryset,many=True)
+            for member in serializer.data:
+                json_data[member.uuid]={
+                    "agentName":member.name,
+                    "salesBooked":_saleRetriever(member.uuid),    
+                }
+            return Response(data=json_data,status=status.HTTP_302_FOUND)
+        except ObjectDoesNotExist as exc:
+            pass
+    def retrieve(self,request,uuid=None):
+        try:
+            queryset=SalesData.objects.get_queryset(agent=uuid)
+            if queryset is None:
+                return Response({"msg":"No Sales Found"},status=status.HTTP_404_NOT_FOUND)
+            else:
+                serializer=_SaleSerializer(queryset,many=True)
+                return Response(serializer.data,status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            ...
+
