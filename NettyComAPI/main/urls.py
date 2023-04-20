feaa6ebfaf251b27f621d7a4dd070bee577571a9
Wali-Muhammad123 <renedescartian@gmail.com>
2023-04-20 03:51:28 -0400

    Added router in urls.py

--- NettyComAPI/main/urls.py
+++ NettyComAPI/main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from .views import index, TeamView
+from .views import index, TeamView, SaleView, TeamSales
+from rest_framework.routers import DefaultRouter
+r=DefaultRouter()
+r.register(prefix='teamsales',viewset=TeamSales,basename='teamsales')
 urlpatterns=[
     path('',index,name='index'),
     path('team/',TeamView.as_view(),name='createteam'),
+    path('sales/',SaleView.as_view(),name='sales'),
 ]
